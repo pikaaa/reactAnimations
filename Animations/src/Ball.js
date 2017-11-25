dcb2682f53c88f44bd4ccc6a47249764dc9f59ef
Viren Malhan <viren.malhan@bookmyshow.com>
2017-11-25 19:25:28 +0530

    initial

---
+++ Animations/src/Ball.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
+
+class Ball extends Component{
+  render(){
+    return(
+      <View style={styles.ball}/>
+    );
+  }
+}
+
+const styles=StyleSheet.create({
+  container:{
+
+  },
+  ball:{
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    borderWidth: 30,
+    borderColor: 'black'
+  }
+});
+
+export default Ball;
