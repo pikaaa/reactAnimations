b67db66cfacab9abbff98381df0b45a7c72eaa59
Viren Malhan <viren.malhan@bookmyshow.com>
2017-11-26 19:01:39 +0530

    setting up pan responder

--- Animations/src/deck.js
+++ Animations/src/deck.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { View,
   Text,
   Animated,
+  PanResponder,
   StyleSheet,
   TouchableOpacity,
   Image } from 'react-native';
 
   class Deck extends Component{
+    constructor(props){
+      super(props);
+      const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => {},
+        onPanResponderMove: (event, gesture) => {
+          console.log(gesture);
+        },
+        onPanResponderRelease: () => {}
+      });
+      // this.panResponder = panResponder;
+      this.state = {panResponder}; //Its seen but not a good practice, creates confusion
+    }
+
     renderCards(){
         return this.props.data.map(item => {
           return this.props.renderCard(item);
